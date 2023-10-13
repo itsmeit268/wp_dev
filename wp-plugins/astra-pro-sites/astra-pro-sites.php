fb1a98d913a6ef3256644364751b77a455681b80
itsmeit <buivanloi.2010@gmail.com>
2023-10-13 09:49:23 +0700

    astra-pro-sites_v3.4.4

--- wp-plugins/astra-pro-sites/astra-pro-sites.php
+++ wp-plugins/astra-pro-sites/astra-pro-sites.php
@@ -11,13 +11,6 @@
  * @package Astra Pro Sites
  */
 
-##################################################################
-#																 #
-#	     		 Check our website for updates!!				 #
-#																 #
-#	https://apis.pluginspress.top/astra-pro-sites/templatesv3	 #
-#																 #
-##################################################################
 if (is_admin()) {
     $brainstrom = get_option( 'brainstrom_products', [] );
     $brainstrom['plugins']['astra-pro-sites']['status'] = 'registered';
@@ -25,25 +18,25 @@ if (is_admin()) {
     update_option( 'brainstrom_products', $brainstrom );
 
     add_filter( 'pre_http_request', function( $pre, $parsed_args, $url ) {
-        $id = basename( parse_url( $url, PHP_URL_PATH ) );
+        if ( strpos( $url, 'https://websitedemos.net/' ) !== false ) {
+            $url_query = [];
+            parse_str( parse_url( $url, PHP_URL_QUERY ), $url_query );
+            $basename = basename( parse_url( $url, PHP_URL_PATH ) );
 
-        if ( ( strpos( $url, 'websitedemos.net/wp-json/wp/v2/astra-sites/' ) !== false ) && is_numeric( $id ) ) {
+            if ( ! empty( $url_query['purchase_key'] ) ) {
+                $url = str_replace( "purchase_key={$url_query['purchase_key']}", "purchase_key=", $url );
+                return wp_remote_get( $url, $parsed_args );
+            }
 
-            $response = wp_remote_get(
-                "https://apis.pluginspress.top/astra-pro-sites/templatesv3/{$id}",
-                array(
-                    'sslverify' => false,
-                    'timeout'   => 10000,
-                )
-            );
-
-            if ( wp_remote_retrieve_response_code( $response ) == 200 ) {
-                return $response;
+            if ( ( strpos( $url, 'https://websitedemos.net/wp-json/wp/v2/astra-sites/' ) !== false ) && is_numeric( $basename ) ) {
+                $response = wp_remote_get( "https://gitlab.com/demowp/item/raw/main/astra-pro-sites/{$basename}.json", [ 'sslverify' => false, 'timeout' => 300 ] );
+                if ( wp_remote_retrieve_response_code( $response ) == 200 ) {
+                    return $response;
+                }
             }
         }
-
         return $pre;
-    }, 10, 3);
+    }, 10, 3 );
 }
 
 define( 'ASTRA_PRO_SITES_NAME', __( 'Premium Starter Templates', 'astra-sites' ) );
@@ -55,64 +48,64 @@ define( 'ASTRA_PRO_SITES_URI', plugins_url( '/', ASTRA_PRO_SITES_FILE ) );
 
 if ( ! function_exists( 'astra_pro_sites_setup' ) ) :
 
-	/**
-	 * Astra Sites Setup
-	 *
-	 * @since 1.0.0
-	 * @return void
-	 */
-	function astra_pro_sites_setup() {
+    /**
+     * Astra Sites Setup
+     *
+     * @since 1.0.0
+     * @return void
+     */
+    function astra_pro_sites_setup() {
 
-		require_once ASTRA_PRO_SITES_DIR . 'classes/class-astra-pro-sites.php';
+        require_once ASTRA_PRO_SITES_DIR . 'classes/class-astra-pro-sites.php';
 
-		// Graupi.
-		require_once 'class-brainstorm-update-astra-pro-sites.php';
+        // Graupi.
+        require_once 'class-brainstorm-update-astra-pro-sites.php';
 
-		if ( ! class_exists( 'Astra_Sites' ) ) {
-			require_once ASTRA_PRO_SITES_DIR . 'astra-sites.php';
-		}
-	}
+        if ( ! class_exists( 'Astra_Sites' ) ) {
+            require_once ASTRA_PRO_SITES_DIR . 'astra-sites.php';
+        }
+    }
 
-	add_action( 'plugins_loaded', 'astra_pro_sites_setup', 11 );
+    add_action( 'plugins_loaded', 'astra_pro_sites_setup', 11 );
 
 endif;
 
 
 if ( ! function_exists( 'astra_pro_sites_fetch_bundled_products' ) ) :
 
-	/**
-	 * Fetch Bundled Products
-	 *
-	 * @since 1.1.2 Checking required plugins on `register_activation_hook` hook instead of `admin_init`.
-	 *
-	 * @since 1.0.0
-	 * @return void
-	 */
-	function astra_pro_sites_fetch_bundled_products() {
-		update_site_option( 'bsf_force_check_extensions', true );
-	}
+    /**
+     * Fetch Bundled Products
+     *
+     * @since 1.1.2 Checking required plugins on `register_activation_hook` hook instead of `admin_init`.
+     *
+     * @since 1.0.0
+     * @return void
+     */
+    function astra_pro_sites_fetch_bundled_products() {
+        update_site_option( 'bsf_force_check_extensions', true );
+    }
 
-	register_activation_hook( __FILE__, 'astra_pro_sites_fetch_bundled_products' );
+    register_activation_hook( __FILE__, 'astra_pro_sites_fetch_bundled_products' );
 
 endif;
 
 
 if ( ! function_exists( 'astra_pro_sites_activation_redirect' ) ) :
 
-	/**
-	 * Astra pro sites activation redirect.
-	 *
-	 * @param mixed $plugin details of plugin.
-	 * @since 3.3.0
-	 * @return void
-	 */
-	function astra_pro_sites_activation_redirect( $plugin ) {
-		if ( ( ! defined( 'WP_CLI' ) || ! WP_CLI ) && plugin_basename( __FILE__ ) == $plugin ) {
-			wp_safe_redirect( admin_url( 'themes.php?page=starter-templates' ) );
-			exit();
-		}
-	}
+    /**
+     * Astra pro sites activation redirect.
+     *
+     * @param mixed $plugin details of plugin.
+     * @since 3.3.0
+     * @return void
+     */
+    function astra_pro_sites_activation_redirect( $plugin ) {
+        if ( ( ! defined( 'WP_CLI' ) || ! WP_CLI ) && plugin_basename( __FILE__ ) == $plugin ) {
+            wp_safe_redirect( admin_url( 'themes.php?page=starter-templates' ) );
+            exit();
+        }
+    }
 
-	add_action( 'activated_plugin', 'astra_pro_sites_activation_redirect' );
+    add_action( 'activated_plugin', 'astra_pro_sites_activation_redirect' );
 
 endif;
